3226e6d062e8430bb0efcfc9e318384d1ca95bd6
Naimur Reza <naimur.rezaa@gmail.com>
2023-05-25 23:17:23 +0600

    added onAuthSTate change

--- providers/AuthProviders.jsx
+++ providers/AuthProviders.jsx
@@ -1,9 +1,18 @@
 import React, { createContext, useState } from "react";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
 export const AuthContext = createContext();
+const auth = getAuth();
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
   const authInfo = {
     user,
   };
