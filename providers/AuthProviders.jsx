903007fc89974b24b4c7f0b2e500aaabbebbd5aa
Naimur Reza <naimur.rezaa@gmail.com>
2023-05-25 23:42:52 +0600

    done with auth providers info

--- providers/AuthProviders.jsx
+++ providers/AuthProviders.jsx
@@ -1,11 +1,36 @@
 import React, { createContext, useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getAuth,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { useEffect } from "react";
 export const AuthContext = createContext();
 const auth = getAuth();
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  //  create user with email and password
+  const createUser = (email, password) => {
+    setLoading(false);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // sign in with email and password
+  const signIn = (email, password) => {
+    setLoading(false);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //  log Out
+  const logOut = () => {
+    setLoading(false);
+    return signOut(auth);
+  };
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
@@ -15,6 +40,10 @@ const AuthProviders = ({ children }) => {
   }, []);
   const authInfo = {
     user,
+    loading,
+    createUser,
+    signIn,
+    signOut,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
