8834bf9a04e55772af81813b000d79620d3f1903
Naimur Reza <naimur.rezaa@gmail.com>
2023-05-29 19:38:14 +0600

    admin role and delete part done

--- src/Components/Shared/Spinner.jsx
+++ src/Components/Shared/Spinner.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { ImSpinner9 } from "react-icons/im";
+import { ScrollRestoration } from "react-router-dom";
 const Spinner = () => {
   return (
     <>
+      <ScrollRestoration />
       <div className="min-h-[calc(100vh-58px)]  flex items-center justify-center">
         <ImSpinner9 size={47} className="animate-spin text-red-500  " />
       </div>
